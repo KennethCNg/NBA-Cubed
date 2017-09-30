57ab40b75660ad013af87b082231ad2344428943
Kenneth Ng <kenneth.ng.4@gmail.com>
2017-09-30 03:17:13 -0400

    get webpack running

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+module.exports = {
+  context: __dirname,
+  entry: './lib/main.js',
+  output: {
+    path: path.resolve(__dirname),
+    filename: './app/assets/javascripts/bundle.js',
+  },
+
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
