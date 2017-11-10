cab6297cac1a1c99da4a2e7ae155db29e8ba76ab
Kenneth Ng <kenneth.ng.4@gmail.com>
2017-11-10 18:32:15 -0500

    delete unnecessary module

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -70,6 +70,8 @@
 "use strict";
 
 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
 var _three = __webpack_require__(1);
 
 var THREE = _interopRequireWildcard(_three);
@@ -82,6 +84,8 @@ var cubeArr = [];
 
 init();
 animate();
+
+console.log(typeof OrbitControls === "undefined" ? "undefined" : _typeof(OrbitControls));
 
 function init() {
 
