fb365fb90186820d3318162a7416ce203f294dff
Kenneth Ng <kenneth.ng.4@gmail.com>
2017-11-30 14:32:47 -0500

    change api to real-time updates

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -44314,7 +44314,7 @@ Object.defineProperty(exports, "__esModule", {
 var fetchPlayers = exports.fetchPlayers = function fetchPlayers() {
   return $.ajax({
     type: "GET",
-    url: "https://api.mysportsfeeds.com/v1.1/pull/nba/2016-2017-regular/cumulative_player_stats.json?playerstats=2PM,3PM,FTM",
+    url: "https://api.mysportsfeeds.com/v1.1/pull/nba/2017-2018-regular/cumulative_player_stats.json?playerstats=2PM,3PM,FTM",
     dataType: 'json',
     async: false,
     headers: {
